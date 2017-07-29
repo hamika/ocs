7b099bdd063adb1ba94e69fa278c5f0f6a2bc966
福崎 結生 <youfose@gmail.com>
2017-07-29 17:00:57 +0900

    User => ActionPlan Controller changes

--- app/controllers/action_plans_controller.rb
+++ app/controllers/action_plans_controller.rb
@@ -1,9 +1,11 @@
 class ActionPlansController < ApplicationController
   before_action :set_action_plan, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /action_plans
   # GET /action_plans.json
   def index
+    puts "イ ン デ ッ ク ス テ ス ト テ ス ト ホ ゲ ホ ゲ 〜"
     @action_plans = ActionPlan.all
   end
 
@@ -14,19 +16,29 @@ class ActionPlansController < ApplicationController
 
   # GET /action_plans/new
   def new
+    puts "ニ ュ ー 〜 ー テ ス ト テ ス ト ホ ゲ ホ ゲ 〜"
+    @user = current_user.id
+    puts "#{@user} hogehgoe!!!!!"
     @action_plan = ActionPlan.new
   end
 
   # GET /action_plans/1/edit
   def edit
+    puts "エ デ ィ ッ っ ト テ ス ト テ ス ト ホ ゲ ホ ゲ 〜"
   end
 
   # POST /action_plans
   # POST /action_plans.json
   def create
+    puts "ク リ エ イ ト テ ス ト テ ス ト ホ ゲ ホ ゲ 〜"
     @action_plan = ActionPlan.new(action_plan_params)
 
     respond_to do |format|
+      # ap = ActionPlan.find(params[:user_id])
+      # ap = current_user
+      # puts "#{@user}"
+      # ap.save
+      @action_plan.user_id = current_user.id
       if @action_plan.save
         format.html { redirect_to @action_plan, notice: 'Action plan was successfully created.' }
         format.json { render :show, status: :created, location: @action_plan }
@@ -40,6 +52,7 @@ class ActionPlansController < ApplicationController
   # PATCH/PUT /action_plans/1
   # PATCH/PUT /action_plans/1.json
   def update
+    puts "ア ッ プ デ イ ー ト テ ス ト テ ス ト ホ ゲ ホ ゲ 〜"
     respond_to do |format|
       if @action_plan.update(action_plan_params)
         format.html { redirect_to @action_plan, notice: 'Action plan was successfully updated.' }
@@ -54,6 +67,7 @@ class ActionPlansController < ApplicationController
   # DELETE /action_plans/1
   # DELETE /action_plans/1.json
   def destroy
+    puts "デ ス ト ロ イ テ ス ト テ ス ト ホ ゲ ホ ゲ 〜"
     @action_plan.destroy
     respond_to do |format|
       format.html { redirect_to action_plans_url, notice: 'Action plan was successfully destroyed.' }
