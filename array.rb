f6af6fdf1eaaa449267f863996bc6b1817e0aee5
福崎 結生 <youfose@gmail.com>
2017-08-11 10:27:43 +0900

    array.rb

--- array.rb
+++ array.rb
@@ -5,18 +5,22 @@ array.each { |i| result += i }
 puts "結果1: #{result}"
 
 array  =  [4, 6, 7, 9]
-result =  0
-array.each do |i|
-  result += i
-end
-puts "結果2: #{result}"
-
-array  =  [4, 6, 7, 9]
 result =  array.inject(:+)
-puts "結果3: #{result}"
+puts "結果2: #{result}"
 
 # ハッシュが入っている配列の中身を合計する
 array  =  [{value: 4}, {value: 6}, {value: 7}, {value: 9}]
 result =  0
 result =  array.map { |i| i[:value] }.inject(:+)
+puts "結果3: #{result}"
+
+array  =  [{value: 4}, {value: 6}, {value: 7}, {value: 9}]
+result =  0
+array.each { |i| result += i[:value] }
 puts "結果4: #{result}"
+
+array  = [{value: 4}, {value: 6}, {value: 7}, {value: 9}]
+result = []
+array.each { |i| result << i[:value] }
+result = result.inject(:+)
+puts "結果5: #{result}"
