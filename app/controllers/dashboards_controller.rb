ef1acdd0172cb4baa966cf3e8a44c49b5ae6ce5b
福崎 結生 <youfose@gmail.com>
2017-08-11 13:48:25 +0900

    small changes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,10 +6,13 @@ class DashboardsController < ApplicationController
   def index
     puts "イ ン デ ッ ク ス テ ス ト テ ス ト ホ ゲ ホ ゲ 〜"
     # ar&rubyをいじるだけ！paramsとかとりあえずつS買わないかな！
-    Schedule.where(action_plan_id: 5).select(:target).map do |schedule|
-      array = []
-      hash = schedule.slice(:target).flatten
-      p hash
+    schedules = Schedule.where(action_plan_id: 5).select(:target, :achievement)
+    array = []
+    schedules.each do |i|
+      array << i[:target]
+      array << i[:achievement]
+      # array = array.inject(:target)
+      p array
     end
   end
 end
