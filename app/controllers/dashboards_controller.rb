cef10b19a2babd129f1f87da117905d6a205bda5
福崎 結生 <youfose@gmail.com>
2017-08-19 16:07:58 +0900

    put calc processing of Schedule table / ActionPlan table | #5

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,14 +5,23 @@ class DashboardsController < ApplicationController
   # GET /dashboards.json
   def index
     puts "イ ン デ ッ ク ス テ ス ト テ ス ト ホ ゲ ホ ゲ 〜"
-    # ar&rubyをいじるだけ！paramsとかとりあえずつS買わないかな！
-    schedules = Schedule.where(action_plan_id: 5).select(:target, :achievement)
+    num = 5
     array = []
-    schedules.each do |i|
-      array << i[:target]
-      array << i[:achievement]
-      # array = array.inject(:target)
-      p array
+    schedules = Schedule.where(action_plan_id: num)
+    schedules.each do |schedule|
+      array << [schedule[:target], schedule[:achievement]]
     end
+
+    t_result = 0
+    a_result = 0
+    array.each do |ary|
+      t_result += ary[0]
+      a_result += ary[1]
+    end
+
+    sum_up = {}
+    action_plan = ActionPlan.find(num)
+    sum_up[:target] = t_result / action_plan[:target].to_f
+    sum_up[:achievement] = a_result / action_plan[:achievement].to_f
   end
 end
