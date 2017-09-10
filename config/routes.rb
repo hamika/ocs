a836eb409d257d132934ef3ec94d212148ce20da
福崎 結生 <youfose@gmail.com>
2017-09-10 14:33:29 +0900

    root to: の設定 | #19

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'dashboards', to: 'dashboards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'schedules#index'
 
   resource :mypage
 end
